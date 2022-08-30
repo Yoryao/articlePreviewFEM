79bbd033adebcb108dfae6de890c2fb784c64913
Jorge Rivera <jrivera@VESSELARG.local>
2022-08-29 23:11:25 -0300

    css almost ready - next app.

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ console.log("app Working")
 
 
 
-
+let iconContainer = document.getElementById("iconContainer");
+console.log(iconContainer)
 
 
 function showShareIcons() {
     console.log("mostrando redes")
 }
+
