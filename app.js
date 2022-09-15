17d04ebf7a3ef149f6acbcb990d488774ccdcb1e
Jorge Rivera <jrivera@VESSELARG.local>
2022-09-14 22:25:39 -0300

    small fixes to code

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ console.log("app Working")
 
 function showShareIcons() {
     let icons = document.getElementById("icons");
-    console.log("mostrando redes")
-    console.log({icons})
     icons.classList.toggle("hidden")
 }
 
-f
+
 
